abc43d45f052af6a359494e4dec9637a178dfa73
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-09-15 15:01:28 +0400

    add token minters

--- contracts/AvartaToken.sol
+++ contracts/AvartaToken.sol
@@ -1,4 +1,42 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.7.0;
-import "@openzeppelin/contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import {AvartaTokenMinters} from "./AvartaTokenMinters.sol";
+import {Ownable} from "./libs/Ownable.sol";
+import {SafeERC20} from "./libs/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+contract AvartaToken is Ownable, ERC20Upgradeable, AvartaTokenMinters {
+    event DestroyedBlackFunds(address _blackListedUser, uint256 _balance);
+
+    event AddedBlackList(address _user);
+
+    event RemovedBlackList(address _user);
+
+    mapping(address => bool) public isBlackListed;
+
+    function initialize() public initializer {}
+
+    function getBlackListStatus(address _maker) external view returns (bool) {
+        return isBlackListed[_maker];
+    }
+
+    function addBlackList(address _evilUser) public onlyOwner {
+        isBlackListed[_evilUser] = true;
+        emit AddedBlackList(_evilUser);
+    }
+
+    function removeBlackList(address _clearedUser) public onlyOwner {
+        isBlackListed[_clearedUser] = false;
+        emit RemovedBlackList(_clearedUser);
+    }
+
+    function destroyBlackFunds(address _blackListedUser) public onlyOwner {
+        require(isBlackListed[_blackListedUser]);
+        uint256 dirtyFunds = balanceOf(_blackListedUser);
+        _balances[_blackListedUser] = 0;
+        _totalSupply -= dirtyFunds;
+        emit DestroyedBlackFunds(_blackListedUser, dirtyFunds);
+    }
+}
